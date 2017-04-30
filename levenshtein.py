4b08b60fb33b93fb3623fe03075b36c0a6e1c682
pkozyulina <polykoz@gmail.com>
2017-04-30 16:13:52 +0300

    29.04.2017. Two classes, alignment score

--- levenshtein.py
+++ levenshtein.py
@@ -29,29 +29,6 @@ def main(str1, str2):
 from Bio.pairwise2 import format_alignment
 
 
-'''
-def levenshtein(ref, seq):
-    import numpy as np
-
-    if len(ref) < len(seq):
-        return levenshtein(seq, ref)
-    if len(seq) == 0:
-        return len(ref)
-
-    ref = np.array(tuple(ref))
-    seq = np.array(tuple(seq))
-
-    previous_row = np.arange(seq.size + 1)
-    for s in ref:
-        current_row = previous_row + 1
-        current_row[1:] = np.minimum(current_row[1:], np.add(previous_row[:-1], seq != s))
-        current_row[1:] = np.minimum(current_row[1:], current_row[0:-1] + 1)
-        previous_row = current_row
-
-    return previous_row[-1] #- (len(ref) - len(seq))
-'''
-
-
 
 if __name__ == '__main__':
 
